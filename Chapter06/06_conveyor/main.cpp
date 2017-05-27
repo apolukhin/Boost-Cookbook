344c40a0039754879eebe9e73cd630a475184efb
Antony Polukhin <antoshkka@gmail.com>
2017-05-27 11:58:39 +0300

    Sync chapter06 recipe 6

--- Chapter06/06_conveyor/main.cpp
+++ Chapter06/06_conveyor/main.cpp
@@ -1,47 +1,226 @@
-#include <vector>
 struct data_packet { unsigned int value; };
 struct decoded_data { unsigned int value; };
 struct compressed_data { unsigned int value; };
 
 #include <boost/atomic.hpp>
+#include <boost/noncopyable.hpp>
 typedef boost::atomic<unsigned int> atomic_t;
 
-class subsystem1 {
+class subsystem1: boost::noncopyable {
     atomic_t i_;
 public:
-    subsystem1() :
-        i_(0)
+    subsystem1();
+
+    data_packet get_data();
+
+    static const unsigned int max_runs = 10000;
+
+    bool is_stopped() const;
+};
+
+class subsystem2: boost::noncopyable {
+    atomic_t i_;
+public:
+    subsystem2();
+
+    void send_data(const compressed_data& data);
+    unsigned int send_packets_count() const;
+};
+
+
+#include <boost/thread/thread.hpp>
+
+subsystem1 subs1;
+subsystem2 subs2;
+
+decoded_data decode_data(const data_packet& packet);
+compressed_data compress_data(const decoded_data& packet);
+
+void process_data() {
+    while (!subs1.is_stopped()) {
+        data_packet data = subs1.get_data();
+        decoded_data d_decoded = decode_data(data);
+        compressed_data c_data = compress_data(d_decoded);
+        subs2.send_data(c_data);
+    }
+}
+
+void run_in_multiple_threads() {
+    boost::thread t(&process_data);
+    process_data();
+
+    t.join();
+}
+
+//////////////
+
+// class work_queue from chapter 5
+#include <deque>
+#include <boost/function.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/thread/condition_variable.hpp>
+
+class work_queue {
+public:
+    typedef boost::function<void()> task_type;
+
+private:
+    std::deque<task_type>       tasks_;
+    boost::mutex                mutex_;
+    boost::condition_variable   cond_;
+    bool                        is_stopped_;
+
+public:
+    work_queue()
+        : is_stopped_(false)
     {}
 
-    data_packet get_data() {
-        data_packet ret = { ++ i_ };
-        return ret;
-    }
-
-    static const unsigned int max_runs = 10000; // 10 000 runs
-
-    bool is_stopped() const {
-        return i_ == max_runs; // 10 000 runs
-    }
+    void run();
+    void stop();
+    
+    // Same as in Chapter 5, but with
+    // rvalue references support.
+    void push_task(task_type&& task);
 };
+
+//////////////
+
+// global variables
+work_queue decoding_queue, compressing_queue, sending_queue;
+
+void start_data_accepting();
+void do_decode(const data_packet& packet);
+void do_compress(const decoded_data& packet);
+
+void start_data_accepting() {
+    while (!subs1.is_stopped()) {
+        data_packet packet = subs1.get_data();
+
+        decoding_queue.push_task(
+            [packet]() {
+                do_decode(packet);
+            }
+        );
+    }
+}
+
+void do_decode(const data_packet& packet) {
+    decoded_data d_decoded = decode_data(packet);
+
+    compressing_queue.push_task(
+        [d_decoded]() {
+            do_compress(d_decoded);
+        }
+    );
+}
+
+void do_compress(const decoded_data& packet) {
+    compressed_data c_data = compress_data(packet);
+        
+    sending_queue.push_task(
+        [c_data]() {
+            subs2.send_data(c_data);
+        }
+    );
+}
+
+
+#include <boost/thread/thread.hpp>
+int main() {
+    boost::thread t_data_decoding(
+        []() { decoding_queue.run(); }
+    );
+    boost::thread t_data_compressing(
+        []() { compressing_queue.run(); }
+    );
+    boost::thread t_data_sending(
+        []() { sending_queue.run(); }
+    );
+
+    start_data_accepting();
+
+    decoding_queue.stop();
+    t_data_decoding.join();
+
+    compressing_queue.stop();
+    t_data_compressing.join();
+
+    sending_queue.stop();
+    t_data_sending.join();
+
+    assert(subs2.send_packets_count() == subsystem1::max_runs);
+
+
+
+    //run_in_multiple_threads();
+}
+
+
+void work_queue::stop() {
+    boost::lock_guard<boost::mutex> lock(mutex_);
+    is_stopped_ = true;
+    cond_.notify_all();
+}
+
+void work_queue::run() {
+    while (1) {
+        boost::unique_lock<boost::mutex> lock(mutex_);
+        while (tasks_.empty()) {
+            if (is_stopped_) {
+                return;
+            }
+            cond_.wait(lock);
+        }
+
+        task_type t = std::move(tasks_.front());
+        tasks_.pop_front();
+        lock.unlock();
+        
+        t();
+    }
+}
+
+
+
+void work_queue::push_task(task_type&& task) {
+    boost::unique_lock<boost::mutex> lock(mutex_);
+    if (is_stopped_) {
+        return;
+    }
+    tasks_.push_back(std::move(task));
+    lock.unlock();
+    cond_.notify_one();
+}
+
+subsystem1::subsystem1()
+    : i_(0)
+{}
+
+data_packet subsystem1::get_data() {
+    data_packet ret = { ++ i_ };
+    return ret;
+}
+
+  
+bool subsystem1::is_stopped() const {
+    return i_ == max_runs; // 10 000 runs
+}
 
 #include <cassert>
-class subsystem2 {
-    atomic_t i_;
-public:
-    subsystem2() :
-        i_(0)
-    {}
 
-    void send_data(const compressed_data& data) {
-        ++ i_;
-        assert(data.value == i_);
-    }
+subsystem2::subsystem2()
+    : i_(0)
+{}
 
-    unsigned int send_packets_count() const {
-        return i_;
-    }
-};
+void subsystem2::send_data(const compressed_data& data) {
+    ++ i_;
+    assert(data.value == i_);
+}
+
+unsigned int subsystem2::send_packets_count() const {
+    return i_;
+}
 
 decoded_data decode_data(const data_packet& packet) {
     static unsigned int i = 0;
@@ -59,161 +238,3 @@ compressed_data compress_data(const decoded_data& packet) {
     return ret;
 }
 
-
-// class work_queue from chapter 5
-#include <deque>
-#include <boost/function.hpp>
-#include <boost/thread/mutex.hpp>
-#include <boost/thread/locks.hpp>
-#include <boost/thread/condition_variable.hpp>
-
-class work_queue {
-public:
-    typedef boost::function<void()> task_type;
-
-private:
-    std::deque<task_type>   tasks_;
-    boost::mutex            mutex_;
-    boost::condition_variable cond_;
-    bool                    is_stopped_;
-
-public:
-    work_queue()
-        : is_stopped_(false)
-    {}
-
-    void stop() {
-        boost::lock_guard<boost::mutex> lock(mutex_);
-        is_stopped_ = true;
-        cond_.notify_all();
-    }
-    
-    void push_task(const task_type& task) {
-        boost::unique_lock<boost::mutex> lock(mutex_);
-        if (is_stopped_) {
-            return;
-        }
-        tasks_.push_back(task);
-        lock.unlock();
-        cond_.notify_one();
-    }
-
-    task_type pop_task() {
-        boost::unique_lock<boost::mutex> lock(mutex_);
-        while (tasks_.empty()) {
-            if (is_stopped_) {
-                return task_type();
-            }
-            cond_.wait(lock);
-        }
-
-        task_type ret = tasks_.front();
-        tasks_.pop_front();
-
-        return ret;
-    }
-};
-
-//#define RUN_BAD_EXAMPLE
-#ifdef RUN_BAD_EXAMPLE
-// global variables
-tasks_queue queue; 
-subsystem1 subs1;
-subsystem2 subs2;
-
-tasks_queue& operator<< (tasks_queue&, data_packet& data) {
-    decoded_data d_decoded = decode_data(data);
-    compressed_data c_data = compress_data(d_decoded);
-    subs2.send_data(c_data);
-}
-
-void start_data_accepting() {
-    while (!subs1.is_stopped()) {
-        queue << subs1.get_data();
-    }
-}
-
-#include <boost/thread/thread.hpp>
-int main() {
-    // Getting data packets from first device
-    // and putting them to queue
-    boost::thread t(&start_data_accepting);
-
-    // Which data packet will be processed first in
-    // multi-threaded environment?
-    // packet #2 may be processed before packet #1,
-    // no guarantee that packets will be processed in
-    // order of their apperance
-    queue.run_multiple();
-    t.join();
-}
-
-#else
-
-// global variables
-work_queue decoding_queue, compressing_queue, sending_queue;
-subsystem1 subs1;
-subsystem2 subs2;
-
-#include <boost/bind.hpp>
-void do_decode(const data_packet& packet);
-void start_data_accepting() {
-    while (!subs1.is_stopped()) {
-        decoding_queue.push_task(boost::bind(
-            &do_decode, subs1.get_data()
-        ));
-    }
-}
-
-void do_compress(const decoded_data& packet);
-void do_decode(const data_packet& packet) {
-    compressing_queue.push_task(boost::bind(
-        &do_compress, decode_data(packet)
-    ));
-}
-
-
-void do_compress(const decoded_data& packet) {
-    sending_queue.push_task(boost::bind(
-        &subsystem2::send_data,
-        boost::ref(subs2),
-        compress_data(packet)
-    ));
-}
-
-void run_while_not_stopped(work_queue& queue) {
-    work_queue::task_type task;
-    while (task = queue.pop_task()) {
-        task();
-    }
-}
-
-#include <boost/thread/thread.hpp>
-int main() {
-    // Getting data packets from first device and putting them to queue
-    boost::thread t_data_accepting(&start_data_accepting);
-    boost::thread t_data_decoding(
-        &run_while_not_stopped, boost::ref(decoding_queue)
-    );
-    boost::thread t_data_compressing(
-        &run_while_not_stopped, boost::ref(compressing_queue)
-    );
-    boost::thread t_data_sending(
-        &run_while_not_stopped, boost::ref(sending_queue)
-    );
-
-    t_data_accepting.join();
-
-    decoding_queue.stop();
-    t_data_decoding.join();
-
-    compressing_queue.stop();
-    t_data_compressing.join();
-
-    sending_queue.stop();
-    t_data_sending.join();
-
-    assert(subs2.send_packets_count() == subsystem1::max_runs);
-}
-
-#endif
