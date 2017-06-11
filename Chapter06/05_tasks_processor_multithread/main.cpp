e03021291ff3522a7a5c067aa02375c8ab0e7199
Antony Polukhin <antoshkka@gmail.com>
2017-06-11 19:28:41 +0300

    Remove incorrect check (checking that execution progresses is sufficient)

--- Chapter06/05_tasks_processor_multithread/main.cpp
+++ Chapter06/05_tasks_processor_multithread/main.cpp
@@ -15,11 +15,7 @@ int main() {
     for (std::size_t i = 0; i < threads_count; ++i) {
         tasks_processor::push_task(&multythread_test);
     }
-    time_t t1 = time(NULL);
     tasks_processor::start_multiple(threads_count);
-    time_t t2 = time(NULL);
-    // One additional second for some io_service and OS delays
-    assert(t2 - t1 < 1);
 
     tasks_processor::start_multiple();
     tasks_processor::stop();
