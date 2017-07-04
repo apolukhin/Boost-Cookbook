734ee3771e6c55136a51f0126698ca6ae3c8f3cb
Antony Polukhin <antoshkka@gmail.com>
2017-07-04 22:35:03 +0300

    Sync code from recipe 09 in chapter 11

--- Chapter11/09_coroutines/main.cpp
+++ Chapter11/09_coroutines/main.cpp
@@ -1,45 +1,73 @@
-#include <iostream>
-
 struct spinlock_t {
-    bool try_lock() {
-        static int i = 0;
-        return i++ >= 100;
-    }
+    bool try_lock();
 };
 
 struct port_t {
-    bool block_ready() {
-        static int i = 0;
-        return i++ >= 10;
-    }
+    bool block_ready();
 };
 
+template <class T> bool copy_to_buffer(T& /*value*/);
+template <class T> void process(T& /*value*/);
+
+#include <iostream>
 
 #include <boost/coroutine2/coroutine.hpp>
-
 typedef boost::coroutines2::asymmetric_coroutine<std::size_t> corout_t;
 
 struct coroutine_task {
     std::string& result;
 
-    explicit coroutine_task(std::string& result_in)
-        : result(result_in)
+    coroutine_task(std::string& r)
+        : result(r)
     {}
 
-    void operator()(corout_t::pull_type& yield) {
-        while (1) {
-            std::size_t max_characters_to_process = yield.get();
+    void operator()(corout_t::pull_type& yield);
 
-            // Do process some characters.
-            // ...
-            result.resize(result.size() + max_characters_to_process);
-
-            // Returning result, switching back
-            // to main program.
-            yield();
-        } /*while*/
-    }
+private:
+    std::size_t ticks_to_work;
+    void tick(corout_t::pull_type& yield);
 };
+
+
+void coroutine_task::operator()(corout_t::pull_type& yield) {
+    ticks_to_work = yield.get();
+
+    // Prepare buffers.
+    std::string buffer0;
+
+    while (1) {
+        const bool requiers_1_more_copy = copy_to_buffer(buffer0);
+        tick(yield);
+
+        if (requiers_1_more_copy) {
+            std::string buffer1;
+
+            copy_to_buffer(buffer1);
+            tick(yield);
+
+            process(buffer1);
+            tick(yield);
+        }
+
+        process(buffer0);
+        tick(yield);
+    }
+}
+
+void coroutine_task::tick(corout_t::pull_type& yield) {
+    result += 'o';
+
+    if (ticks_to_work != 0) {
+        --ticks_to_work;
+    }
+
+    if (ticks_to_work == 0) {
+        // Switching back to main.
+        yield();
+
+        ticks_to_work = yield.get();
+    }
+}
 
 int main() {
     std::string result;
@@ -52,9 +80,9 @@ int main() {
     port_t port;
 
     while (!spinlock.try_lock()) {
-        // We may do some usefull work, before
+        // We may do some useful work, before
         // attempting to lock a spinlock once more.
-        coroutine(10); // Small delays.
+        coroutine(10); // 10 is the ticks count to run.
     }
     // Spinlock is locked.
     // ...
@@ -62,13 +90,34 @@ int main() {
     result.clear();
 
     while (!port.block_ready()) {
-        // We may do some usefull work, before
+        // We may do some useful work, before
         // attempting to get block of data once more.
-        coroutine(300);  // Bigger delays.
+        coroutine(300);  // 300 is the ticks count to run.
+
         // Do something with `result` variable.
     }
 
     assert(result.size() == 10 * 300);
-
     std::cout << "OK\n";
 }
+
+// details:
+
+template <class T> bool copy_to_buffer(T& /*value*/) {
+    static std::size_t i = 0;
+    ++ i;
+     return i & 1;
+}
+
+template <class T> void process(T& /*value*/) {}
+
+bool spinlock_t::try_lock() {
+    static int i = 0;
+    return i++ >= 100;
+}
+
+bool port_t::block_ready() {
+    static int i = 0;
+    return i++ >= 10;
+}
+
