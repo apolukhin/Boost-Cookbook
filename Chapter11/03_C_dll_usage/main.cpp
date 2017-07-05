e73523b124ba764b9c9469f5bb7c1a4403c95b7a
Antony Polukhin <antoshkka@gmail.com>
2017-07-05 22:47:50 +0300

    Sync Chapter 11 recipe 09

--- Chapter11/03_C_dll_usage/main.cpp
+++ Chapter11/03_C_dll_usage/main.cpp
@@ -5,12 +5,13 @@
 
 int main(int argc, char* argv[]) {
     assert(argc == 2);
+    boost::filesystem::path plugin_path = argv[1];
 
     boost::dll::shared_library plugin(
-        argv[1],
+        plugin_path,
         boost::dll::load_mode::append_decorations
     );
 
-    auto greeter = plugin.get<std::string(const std::string)>("greeter");
+    auto greeter = plugin.get<std::string(const std::string&)>("greeter");
     std::cout << greeter("Sally Sparrow");
 }
