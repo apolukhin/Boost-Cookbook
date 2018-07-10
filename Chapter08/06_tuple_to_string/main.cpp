3ebfb8f31d9b6c7650addd7b29d3ee0dc9e6d58e
Antony Polukhin <antoshkka@gmail.com>
2018-07-10 09:07:21 +0300

    Do not pass noncopyable types into the boost::fusion::for_each

--- Chapter08/06_tuple_to_string/main.cpp
+++ Chapter08/06_tuple_to_string/main.cpp
@@ -1,7 +1,6 @@
 #include <boost/lexical_cast.hpp>
-#include <boost/noncopyable.hpp>
 
-struct stringize_functor: boost::noncopyable {
+struct stringize_functor {
 private:
     std::string& result;
 
